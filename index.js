bf7c18c5241db7f98c8c9c6989822dbe4b763d10
zakynaufal98 <zakynaufal1998@gmail.com>
2022-06-04 02:11:44 +0700

    First Commit

--- index.js
+++ index.js
@@ -6,15 +6,28 @@ const names = ["Halo", "Angel", "Nyoman", "Ketut", "Aisyah"];
 // "1. Nama"
 // "2. Nama"
 // ...
-const sorter = null;
+const sorter = (names, sorting) => {
+  let listName = [];
+  for (let i = 0; i < names.length; i++) {
+    if (sorting(names)) {
+      const nameInside = [i + 1] + ". " + names[i];
+      listName.push(nameInside);
+    }
+  }
+  return listName.sort();
+};
 
 // TODO: Fungsi untuk mengurutkan array of string secara ascending
 // mengembalikan array yang sudah diurutkan
-const sortAscending = null;
+const sortAscending = (urutan) => {
+  return urutan.sort();
+};
 
 // TODO: Fungsi untuk mengurutkan array of string secara descending
 // mengembalikan array yang sudah diurutkan
-const sortDescending = null;
+const sortDescending = (urutan) => {
+  return urutan.sort().reverse();
+};
 
 // ! JANGAN DIMODIFIKASI
 (function main() {
